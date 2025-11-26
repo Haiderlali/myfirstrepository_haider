350f0368929c22b5de4c8c03c5e2358be48e01b6
Haiderlali <ha0128ider@gmail.com>
2025-11-26 21:55:18 +0500

    Delete 1_PythonScipyIntro.py

--- 1_PythonScipyIntro.py
+++
@@ -1,9 +0,0 @@
-
-# Scipy is a scientific computation libraray that uses numpy underneath.It stand for Scientific python and providing more utilities functions for optimization , stats and signal processing.
-
-import scipy as sp
-print(sp.__version__)
-
-
-
-#__________________BEST OF LUCK ____________________#
